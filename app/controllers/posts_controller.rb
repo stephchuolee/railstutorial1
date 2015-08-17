8153b9824455fa1e30033946a056cc28d03eabf5
schlorantics <schuolee@gmail.com>
2015-08-17 21:39:31 +0000

    5. refactoring static individual post routes to dynamic posts#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,36 +1,34 @@
 class PostsController < ApplicationController
-  # def index
-  #   @posts = [
   
-  # end 
+  # def index
+  #   [...]
+  # end
 
 
-  def post0 
-    @post = {
-      title: "Staples",
-      author: "Steph Chuo-Lee", 
-      text: <<-eos.gsub(/\s+/, " ").strip
-        Hot dogs and the TV show 'Friends' are staples 
-        in my life.
-      eos
+  def show 
+    posts = [
+      {
+        title: "Staples",
+        author: "Steph Chuo-Lee", 
+        text: <<-eos.gsub(/\s+/, " ").strip
+          Hot dogs and the TV show 'Friends' are staples 
+          in my life.
+        eos
+      },
+      {
+        title: "Staples, again",
+        author: "Steph Chuo-Lee",
+        text: "Did you know that the 'L' in the Staples logo is a staple?"
+      },
+      {
+        title: "Staples once more",
+        author: "Steph Chuo-Lee", 
+        text: "I took a stapler from my old job and I regret nothing."
       }
+    ]
+    @post = posts[params[:id].to_i]
   end 
 
-  def post1
-    @post = {
-      title: "Staples, again",
-      author: "Steph Chuo-Lee",
-      text: "Did you know that the 'L' in the Staples logo is a staple?"
-      }
-  end 
-
-  def post2
-    @post = {
-      title: "Staples once more",
-      author: "Steph Chuo-Lee", 
-      text: "I took a stapler from my old job and I regret nothing."
-      }
-  end 
-
+  
 
 end
