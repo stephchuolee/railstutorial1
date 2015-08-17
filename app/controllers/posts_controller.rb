bd26d070a0eeb7fc31eecf9470fbeecd259d141e
schlorantics <schuolee@gmail.com>
2015-08-17 20:48:00 +0000

    3. Displaying posts on /posts/index.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,24 @@
 class PostsController < ApplicationController
   def index
+    @posts = [
+      {
+        title: "Staples",
+        author: "Steph Chuo-Lee", 
+        text: <<-eos.gsub(/\s+/, " ").strip
+          Hot dogs and the TV show 'Friends' are staples 
+          in my life.
+        eos
+      },
+      {
+        title: "Staples, again",
+        author: "Steph Chuo-Lee",
+        text: "Did you know that the 'L' in the Staples logo is a staple?"
+      },
+      {
+        title: "Staples once more",
+        author: "Steph Chuo-Lee", 
+        text: "I took a stapler from my old job and I regret nothing."
+      }
+    ]
   end 
 end
