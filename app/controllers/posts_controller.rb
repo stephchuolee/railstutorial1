fa32ecf966cc06c477f8d2457ae858b3bc18d78d
schlorantics <schuolee@gmail.com>
2015-08-17 21:58:35 +0000

    7. moving posts into db via seeds.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,34 +1,11 @@
 class PostsController < ApplicationController
   
-  # def index
-  #   [...]
-  # end
+  def index
+    @posts = Post.all
+  end
 
-
-  def show 
-    posts = [
-      {
-        title: "Staples",
-        author: "Steph Chuo-Lee", 
-        text: <<-eos.gsub(/\s+/, " ").strip
-          Hot dogs and the TV show 'Friends' are staples 
-          in my life.
-        eos
-      },
-      {
-        title: "Staples, again",
-        author: "Steph Chuo-Lee",
-        text: "Did you know that the 'L' in the Staples logo is a staple?"
-      },
-      {
-        title: "Staples once more",
-        author: "Steph Chuo-Lee", 
-        text: "I took a stapler from my old job and I regret nothing."
-      }
-    ]
-    @post = posts[params[:id].to_i]
+  def show
+    @post = Post.find(params[:id])
   end 
-
   
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Post.create!(
+  title: "Staples",
+  author: "Steph Chuo-Lee", 
+  text: <<-eos.gsub(/\s+/, " ").strip
+    Hot dogs and the TV show 'Friends' are staples 
+    in my life.
+  eos
+)
+
+Post.create!(
+  title: "Staples once more",
+  author: "Steph Chuo-Lee", 
+  text: "I took a stapler from my old job and I regret nothing."
+)
+
+Post.create!(
+  title: "Staples, again",
+  author: "Steph Chuo-Lee",
+  text: "Did you know that the 'L' in the Staples logo is a staple?"
+)
